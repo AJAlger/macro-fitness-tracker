c7bfc81368004d70a09d8e4ff0af638cdd237384
Abdullah Alger <abdullahalger@me.com>
2015-01-13 09:04:05 +0000

    Saving config for C9

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,6 @@ var myApp = require('./server.js');
 
 // I chose to write all the paths as variables to use throughout.
 
-
 var paths = {
     sass: 'app/stylesheets/scss/', // Stylesheets folder for SASS
     css: 'app/stylesheets/css/', // Stylesheets folder for CSS
@@ -26,7 +25,7 @@ gulp.task('default', ['express', 'styles', 'scripts', 'watch'], function() {});
 
 // Gulp Task to Run Express Server
 gulp.task('express', function() {
-        myApp.listen(process.env.PORT, process.env.IP);
+        myApp.listen(8080);
 });
 
 // Gulp Task to SASS - Bourbon and Neat are Working
